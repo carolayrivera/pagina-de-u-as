33fd981d3b744225b623075ca5c73fc8ea8d99bc
carolay <149634127+carolayrivera@users.noreply.github.com>
2023-11-08 18:39:08 -0500

    guardado2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-
 const Navbar = () => {
   return (
     <div className="w-full h-20 bg-pink-200">
-        <h2>inicio</h2>
-      
+      <div className=" h-full flex  flex-row justify-end items-center">
+        <div className="w-1/2 flex justify-around"> 
+          <h2>logo</h2>
+          <h2>citas</h2>
+          <h2>galeria</h2>
+          <h2>servicios</h2>
+          <h2>inicio</h2>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
